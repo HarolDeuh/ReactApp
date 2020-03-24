37f5eb3346c161a0b69ca03734e02462f15f2f15
Haroldeuh <harold.aholoukpe@gmail.com>
2020-03-24 23:55:45 +0100

    working on style to video 74

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
 import './App.css';
+import styles from 'styled-components'
 import Person from './Person/Person'
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: #fff;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`
 
 class App extends Component {
 
@@ -68,7 +84,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Je suis une app React</h1>
-        <button onClick={this.togglePersonHandler}>Afficher la personne !</button>
+        <StyledButton alt={this.state.showPerson} onClick={this.togglePersonHandler}>
+          Toggle Persons
+        </StyledButton>
         {persons}
       </div>
     );
--- my-react-app/src/Person/Person.js
+++ my-react-app/src/Person/Person.js
@@ -1,14 +1,27 @@
 import React from 'react'
-import './Person.css'
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+    width: 60%;
+    margin: 10px auto;
+    padding: 10px;
+    border: 1px solid #eee;
+    border-radius: 10px;
+
+    @media (min-width: 500px) {
+        width: 500px;
+    }
+`
+
 
 const person = (props) => {
     return (
-        <div className="Person">
+        <StyledDiv>
             <h4 onClick={props.click}>Présentation de {props.name} : </h4>
             <p>Je suis {props.name} et j'ai {props.age} ans</p>
             <h6>Quel est mon nom ? </h6>
             <input type="text" onChange={props.changed} value={props.name} />
-        </div>
+        </StyledDiv>
     )
 }
 
